bf21f775d343ebe60899eaad76c278dd05589be5
npdev <sashko.dev@gmail.com>
2023-04-23 15:58:28 +0100

    initial installation sanity and next.js

---
+++ frontend/config/client-config.ts
@@ -0,0 +1,8 @@
+const config = {
+  projectId: 'v0tnvh1n',
+  dataset: 'production',
+  apiVersion: '2023-04-16',
+  useCdn: false
+}
+
+export default config
---
+++ frontend/sanity-client.ts
@@ -0,0 +1,4 @@
+import sanityClient from '@sanity/client'
+import clientConfig from '@/config/client-config';
+
+export default sanityClient(clientConfig)
